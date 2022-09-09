aaa490d9d130e53ea4d77669700def013b0e5fff
Elquiasjr <elquiasj@gmail.com>
2022-09-09 20:28:53 -0300

    feat: change the enum/switch case

--- myframe.cpp
+++ myframe.cpp
@@ -10,55 +10,47 @@ Myframe::Myframe(QWidget *parent)
 
 }
 
-
-
-//void ObjectGen::drawObj (){
-//    cout << "hello world" ;
-//}
-
-//void ObjectRec::drawObj (){
-//    rec.setRect(160,120,40,40);
-//}
-
 void Myframe::paintEvent(QPaintEvent *event)
 {
     QFrame::paintEvent(event);
     QPainter painter(this);
     painter.setPen(QPen(Qt::darkCyan, 2));
     QPainter *paints = &painter;
-    rectang->type = 1;
-    point->type = 2;
-    line->type = 3;
+    rectang->type = type_rec;
+    point->type = type_point;
+    line->type = type_line;
     for (ObjectGen *x : displayFile) {
              redirectDraw(x, paints);
         }
 }
 
 void ObjectGen::redirectDraw(ObjectGen *x, QPainter *painter){
-
-    if(x->type == 1){
+    switch(x->type){
+    case type_rec:{
         ObjectRec *re = (ObjectRec*) x;
         painter->drawRect(*re);
         re->setRect(160,120,40,40);
+        break;
     }
-    else if(x->type == 2){
+    case type_point:{
         ObjectPoint *pt = (ObjectPoint*) x;
         painter->drawPoint(*pt);
         pt->setX(175);
         pt->setY(140);
+        break;
     }
-    else if(x->type == 3){
+    case type_line:{
         ObjectLine *ln = (ObjectLine*) x;
         painter->drawLine(*ln);
         ln->setLine(300,500,10,10);
+        break;
+    }
     }
 }
 
 void Myframe::DrawAll(){
     cout << "Entrou no Myfram::ClearForms" << endl;
 
-
-    //
     update();
 }
 
--- myframe.h
+++ myframe.h
@@ -10,60 +10,44 @@
 
 using namespace std;
 
+enum Qtype {type_rec, type_point, type_line};
+
 class ObjectGen {
 public:
-    int type;
-    //virtual void drawObj();
-    virtual ~ObjectGen()
-    { }
+    Qtype type;
     void redirectDraw(ObjectGen *x, QPainter *painter);
 };
 
 class ObjectRec : public QRect, public ObjectGen {
 
 public:
-    int type = 1;
     QRect rec;
-    //void drawObj();
-    virtual ~ObjectRec()
-    { }
 };
 
 class ObjectPoint : public QPoint, public ObjectGen {
 public:
     QPoint point;
-    int type = 2;
-    virtual ~ObjectPoint()
-    { }
 };
 
 class ObjectLine : public QLine, public ObjectGen {
 public:
-    int type = 3;
     QLine line;
-    virtual ~ObjectLine()
-    { }
 };
 
-
-class Myframe : public QFrame, ObjectRec
+class Myframe : public QFrame, ObjectRec, QPainter
 {
     Q_OBJECT
 private:
     ObjectGen *rectang = (ObjectGen*)malloc(sizeof(ObjectGen));
     ObjectGen *point = (ObjectGen*)malloc(sizeof(ObjectGen));
     ObjectGen *line = (ObjectGen*)malloc(sizeof(ObjectGen));
-   QList<ObjectGen *> displayFile = {rectang, point, line};
+    QList<ObjectGen *> displayFile = {rectang, point, line};
 
 public:
     explicit Myframe(QWidget *parent = nullptr);
     void paintEvent(QPaintEvent * event) override;
 
 public slots:
-    //void ClearForms();
-    //void DrawRect();
-    //void DrawPoint();
-    //void DrawLine();
     void DrawAll();
 
 signals:
